d216650560bf41a7811eaa747503d94ba9114433
Reichenau <schildt515@gmail.com>
2025-10-11 22:35:12 +0300

    feat(lab1): Реализовано вычисление RPN выражений

--- lab1_Calculator/src/main.cpp
+++ lab1_Calculator/src/main.cpp
@@ -1,23 +1,24 @@
 ﻿#include <iostream>
-#include <parser.h>
 #include <locale.h>
+#include "parser.h"
+#include "evaluator.h"
+
 void print_tokens(const std::vector<std::string>& tokens) {
     for (const auto& token : tokens) {
         std::cout << token << " ";
     }
     std::cout << std::endl;
 }
-int main()
-{
+int main(){
     setlocale(LC_ALL, "ru");
+    
     std::string expression = "16 + 4 * ( 3 - 1 )";
+    std::vector<std::string> rpn_expression = to_rpn(expression);
+    double result = evaluate_rpn(rpn_expression);
 
     std::cout << expression << std::endl;
 
-    std::vector<std::string> rpn_expression = to_rpn(expression);
-
-    std::cout << "Результат: ";
-    print_tokens(rpn_expression);
+    std::cout << "Результат: " << result;
 
     return 0;
 }
