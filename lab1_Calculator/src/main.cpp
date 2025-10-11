ff5f8981bf909c1b9fc57ae2a751f1bafef70ffd
Reichenau <schildt515@gmail.com>
2025-10-11 21:34:02 +0300

    feat(lab1): Реализован парсер выражений в RPN

--- lab1_Calculator/src/main.cpp
+++ lab1_Calculator/src/main.cpp
@@ -1,8 +1,25 @@
 ﻿#include <iostream>
-
+#include <parser.h>
+#include <locale.h>
+void print_tokens(const std::vector<std::string>& tokens) {
+    for (const auto& token : tokens) {
+        std::cout << token << " ";
+    }
+    std::cout << std::endl;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+    std::string expression = "16 + 4 * ( 3 - 1 )";
+
+    std::cout << expression << std::endl;
+
+    std::vector<std::string> rpn_expression = to_rpn(expression);
+
+    std::cout << "Результат: ";
+    print_tokens(rpn_expression);
+
+    return 0;
 }
 
 
