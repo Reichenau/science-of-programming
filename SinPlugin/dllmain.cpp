147b4c904c7ef33e4fee4b697ae643ec40de3431
Reichenau <schildt515@gmail.com>
2025-10-15 18:17:09 +0300

    feat(lab1): Создал плагин SinPlugin для функции синуса

---
+++ SinPlugin/dllmain.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#define DllExport extern "C" __declspec(dllexport)
+
+// Возврат имени операции 
+DllExport const char* getFunctionName() {
+	return "sin";
+}
+
+// Функция, которая выполняет вычисления 
+DllExport double execute(double operand) {
+	double radians = operand * M_PI / 180.0;
+	return std::sin(radians);
+}
