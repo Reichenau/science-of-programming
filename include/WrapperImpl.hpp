479015bf7cdff71d41f5e2be39ddb67e4f45c5b8
Reichenau <schildt515@gmail.com>
2025-11-21 22:56:25 +0300

    feat(lab2): Скелт шаблонного класса WrapperImpl

---
+++ include/WrapperImpl.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "Wrapper.hpp"
+#include <functional>
+#include <map>
+#include <string>
+#include <iostream>
+
+// Шаблонный класс WrapperImpl.
+template<typename T, typename... Args>
+class WrapperImpl : public Wrapper {
+public:
+    // Объявляем тип указателя на метод класса T
+    using MethodType = int (T::*)(Args...);
+
+    WrapperImpl(T* subj, MethodType method, const std::map<std::string, int>& args)
+        : subject(subj), method(method), arguments(args) {
+    }
+
+    // Реализация метода execute из базового класса
+    int execute(const std::map<std::string, int>& inputArgs) override {
+        std::cout << "[WrapperImpl]" << std::endl;
+        return 0;
+    }
+
+private:
+    T* subject;                 
+    MethodType method;          
+    std::map<std::string, int> arguments; 
+};
