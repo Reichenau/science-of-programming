b70c5a618927bd8ae0179b145d740cffe86e8dc2
Reichenau <schildt515@gmail.com>
2025-11-23 21:46:35 +0300

    feat(lab2): Реализация std::any и void

--- include/WrapperImpl.hpp
+++ include/WrapperImpl.hpp
@@ -5,48 +5,63 @@
 #include <iostream>
 #include <utility> 
 #include <stdexcept>
+#include <any>
+#include <tuple>
+#include <type_traits>
 
 // Шаблонный класс WrapperImpl
-template<typename T, typename... Args>
+// T - тип класса
+// R - тип возарвщаемого значения 
+template<typename T, typename R, typename... Args>
 class WrapperImpl : public Wrapper {
 public:
-    using MethodType = int (T::*)(Args...);
+    using MethodType = R(T::*)(Args...);
 
-    WrapperImpl(T* subj, MethodType method, const std::map<std::string, int>& args)
+    WrapperImpl(T* subj, MethodType method, const std::map<std::string, std::any>& args)
         : subject(subj), method(method), arguments(args) {
         if (subject == nullptr) {
             throw std::invalid_argument("WrapperImpl: Subject pointer cannot be null.");
         }
     }
 
-    int execute(const std::map<std::string, int>& inputArgs) override {
+    std::any execute(const std::map<std::string, std::any>& inputArgs) override {
         return executeInternal(inputArgs, std::index_sequence_for<Args...>{});
     }
 
 private:
-    T* subject;                 
-    MethodType method;          
-    std::map<std::string, int> arguments; 
+    T* subject;
+    MethodType method;
+    std::map<std::string, std::any> arguments;
+    
+    using ArgsTuple = std::tuple<Args...>;
 
     template<size_t... Indices>
-    int executeInternal(const std::map<std::string, int>& inputArgs, std::index_sequence<Indices...>) {
-        return (subject->*method)(getArgumentValue(inputArgs, Indices)...);
+    std::any executeInternal(const std::map<std::string, std::any>& inputArgs, std::index_sequence<Indices...>) {
+        if constexpr (std::is_void_v<R>) {
+            (subject->*method)(getArgumentValue<Indices>(inputArgs)...);
+            return {}; 
+        }
+        else {
+            return (subject->*method)(getArgumentValue<Indices>(inputArgs)...);
+        }
     }
 
     // Получение значения аргумента по индексу
-    int getArgumentValue(const std::map<std::string, int>& inputArgs, size_t index) {
+    template<size_t index>
+    auto getArgumentValue(const std::map<std::string, std::any>& inputArgs) {
+        using ArgType = std::tuple_element_t<index, ArgsTuple>;
         std::string key = "arg" + std::to_string(index + 1);
 
         // 1. Ищем в переданных аргументах 
         auto it = inputArgs.find(key);
         if (it != inputArgs.end()) {
-            return it->second;
+            return std::any_cast<ArgType>(it->second);
         }
 
         // 2. Ищем в аргументах по умолчанию
         it = arguments.find(key);
         if (it != arguments.end()) {
-            return it->second;
+            return std::any_cast<ArgType>(it->second);
         }
 
         // 3. Если не нашли — предупреждение 
