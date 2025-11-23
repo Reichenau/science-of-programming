ac0c7707d43d38650a9c4578a63f8e37a49f39ea
Reichenau <schildt515@gmail.com>
2025-11-23 23:00:14 +0300

    feat(lab2): Добавлен метод Subject::print

--- include/Subject.hpp
+++ include/Subject.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <string>
 
 class Subject {
 public:
     int f3(int a, int b);
+    void print(const std::string& message);
 };
--- src/Subject.cpp
+++ src/Subject.cpp
@@ -3,5 +3,9 @@
 
 int Subject::f3(int a, int b) {
     std::cout << "Subject::f3(" << a << ", " << b << ") called." << std::endl;
-    return a - b; 
+    return a - b;
+}
+
+void Subject::print(const std::string& message) {
+    std::cout << "Subject::print(\"" << message << "\") called." << std::endl;
 }
