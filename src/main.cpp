10f512f83a10699e16a218d32241adaa1acd9c55
Daniel <schildt515@gmail.com>
2025-12-03 23:14:14 +0300

    refactor(lab2): обновление теств в main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,21 @@
 #include "wrapper/WrapperImpl.hpp"
 
 int main() {
-    std::cout << "=== Lab work: Wrapper ===" << std::endl;
-
     Subject subj;
     Engine engine;
 
-    auto wrapper1 = make_wrapper(&subj, &Subject::f3, { {"arg1", 0}, {"arg2", 0} });
-    auto wrapper2 = make_wrapper(&subj, &Subject::print, { {"arg1", std::string("default")} });
+    Wrapper wrapper1(&subj, &Subject::f3, { {"arg1", 0}, {"arg2", 0} });
+    Wrapper wrapper2(&subj, &Subject::print, { {"arg1", std::string("default")} });
 
     try {
-        engine.register_command(std::move(wrapper1), "command1");
-        engine.register_command(std::move(wrapper2), "command2");
+        engine.register_command(&wrapper1, "command1");
+        engine.register_command(&wrapper2, "command2");
 
         // Тест 1 (int)
-        std::any res1 = engine.execute("command1", { {"arg1", 10}, {"arg2", 3} });
-        std::cout << "Result: " << std::any_cast<int>(res1) << std::endl;
+        std::cout << "Result: " << std::any_cast<int>(engine.execute("command1", { {"arg1", 4}, {"arg2", 5} })) << std::endl;
 
         // Тест 2 (string, void)
-        engine.execute("command2", { {"arg1", std::string("Hello, World!")} });
+        engine.execute("command2", { {"arg1", std::string("Hello World!")} });
     }
     catch (const std::exception& e) {
         std::cerr << "Exception caught: " << e.what() << std::endl;
