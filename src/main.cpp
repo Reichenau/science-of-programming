ff51797a40a852f52043a3a67fe09f910d2b91dc
Daniel <schildt515@gmail.com>
2025-12-14 15:11:12 +0300

    refactor(lab2): адаптация main.cpp для работы с умными указателими

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <any>
+#include <memory>
 #include "subject/Subject.hpp"      
 #include "engine/Engine.hpp"        
 #include "wrapper/WrapperImpl.hpp"
@@ -7,16 +8,26 @@
 int main() {
     // Создаем объекты
     Subject subj;
-    Engine engine;
 
-    Wrapper wrapper1(&subj, &Subject::f3, { {"arg1", 0}, {"arg2", 0} });
-    Wrapper wrapper2(&subj, &Subject::print, { {"arg1", std::string("default")} });
+    auto wrapper1 = std::make_unique<Wrapper<Subject, int, int, int>>(
+        &subj,
+        &Subject::f3,
+        std::map<std::string, std::any>{ {"arg1", 0}, { "arg2", 0 } } 
+    );
+
+    auto wrapper2 = std::make_unique<Wrapper<Subject, void, const std::string&>>(
+        &subj,
+        &Subject::print,
+        std::map<std::string, std::any>{ {"arg1", std::string("default")} } 
+    );
+
+    Engine engine;
 
     // Создаем обертки для методов
     try {
         // Регистрируем команды
-        engine.register_command(&wrapper1, "command1");
-        engine.register_command(&wrapper2, "command2");
+        engine.register_command(std::move(wrapper1), "command1");
+        engine.register_command(std::move(wrapper2), "command2");
 
         // Тест 1 (int)
         auto result = engine.execute("command1", { {"arg1", 4}, {"arg2", 5} });
