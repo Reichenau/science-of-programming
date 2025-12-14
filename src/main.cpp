4798d86ba2e6df9dcad3116c7b74710a9accb6f8
Daniel <schildt515@gmail.com>
2025-12-14 15:55:18 +0300

    refactor(lab2): Удаление неиспользуемых заголовков

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 ﻿#include <iostream>
-#include "subject/Subject.hpp"      
-#include "engine/Engine.hpp"        
-#include "wrapper/WrapperImpl.hpp"
 #include "test.hpp"
 
 int main(int argc, char** argv) {
